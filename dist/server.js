89f528615095e5b387cd40cb9bcfba11370d881e
João Luca Amorim Luebke <57159653+joaoluebke@users.noreply.github.com>
2023-01-06 12:47:02 -0300

    build4

--- dist/server.js
+++ dist/server.js
@@ -72,8 +72,8 @@ var user_1 = require("./routes/user");
 var product_1 = require("./routes/product");
 var category_1 = require("./routes/category");
 var subCategory_1 = require("./routes/subCategory");
-var fs_1 = __importDefault(require("fs"));
-var path_1 = __importDefault(require("path"));
+// import fs from "fs";
+// import path from "path";
 dotenv.config();
 function bootstrap() {
     return __awaiter(this, void 0, void 0, function () {
@@ -83,11 +83,7 @@ function bootstrap() {
                 case 0:
                     fastify = (0, fastify_1["default"])({
                         logger: true,
-                        http2: true,
-                        https: {
-                            key: fs_1["default"].readFileSync(path_1["default"].join(__dirname, "../src", "ssl", "code.key")),
-                            cert: fs_1["default"].readFileSync(path_1["default"].join(__dirname, "../src", "ssl", "code.crt"))
-                        }
+                        http2: true
                     });
                     return [4 /*yield*/, fastify.register(fastify_multer_1["default"].contentParser)];
                 case 1:
